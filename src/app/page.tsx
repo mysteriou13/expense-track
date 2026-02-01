e3df997d6df7d7fb5a24ba58d603a261a8438eac
Massa anthony <elio007@hotmail.fr>
2026-02-01 14:20:08 +0100

    typescript formatting file _app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 
-import { store } from "./redux/store";
-import { Provider } from "react-redux";
+
 export default function Home() {
   return (
+    
+ 
     <div className="bg-red-900 p-4">
-      <Provider store={store}>Good</Provider>
+      <div>Good page</div>
     </div>
+  
+    
   );
 }
---
+++ src/pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from "next/app";
+import { Session } from "next-auth";
+import { SessionProvider } from "next-auth/react"
+import { store} from "@/app/redux/store";
+import { Provider } from "react-redux";
+
+interface MyAppProps extends AppProps {
+  pageProps: {
+    session?: Session;
+    [key: string]: any; 
+  };
+}
+
+
+export default function App({Component,pageProps}:MyAppProps) {
+  
+  const {session, ...rest} = pageProps;
+
+  return (
+    <Provider store = {store}>
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+    </Provider>
+
+  )
+}
