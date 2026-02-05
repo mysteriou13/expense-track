8f41786487d804c303fe7bffa0a127f3f2ba7f10
Massa anthony <elio007@hotmail.fr>
2026-02-05 16:37:12 +0100

    add arrow and TitleIndex in user page

--- src/app/User/page.tsx
+++ src/app/User/page.tsx
@@ -1,10 +1,21 @@
 import SideMenu from "@/app/components/SideMenu/SideMenu";
+import Arrow from "@/app/components/Arrow/Arrow";
+import TitleIIndex from "@/app/components/TitleIndex/TitleIIndex";
 
 export default async function page() {
-   
   return (
     <div className="bg-[#1B1B1B] h-screen">
-    <SideMenu/>
+      <div>
+        <div>
+          <SideMenu />
+        </div>
+        
+        <div className="relative left-5 top-[35vh]">
+          <Arrow />
+          <TitleIIndex />
+        </div>
+
+      </div>
     </div>
   );
 }
