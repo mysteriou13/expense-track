c8c9ed5aad49395b91cb5712fb32b151c26986a9
Massa anthony <elio007@hotmail.fr>
2026-02-05 07:11:36 +0100

    delette session in _app.tsx

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,22 +1,13 @@
 import { AppProps } from "next/app";
-import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { store } from "@/app/redux/store";
 import { Provider } from "react-redux";
 
-interface MyAppProps extends AppProps {
-  pageProps: {
-    session?: Session;
-    [key: string]: any;
-  };
-}
 
-export default function App({ Component, pageProps }: MyAppProps) {
-  const { session } = pageProps;
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <SessionProvider session={session}>
+      <SessionProvider session={pageProps}>
         <Component {...pageProps} />
       </SessionProvider>
     </Provider>
